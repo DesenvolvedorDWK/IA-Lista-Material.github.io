973955fa0b1d01b2bf7041411d62bc0e026cd34c
DesenvolvedorDWK <111389400+DesenvolvedorDWK@users.noreply.github.com>
2026-09-11 11:17:52 -0300

    Update rfid.js

--- rfid.js
+++ rfid.js
@@ -1,36 +1,3 @@
 function abrirRFID() {
-
-    alert(
-        "Módulo RFID conectado."
-    );
+    console.log("RFID pronto para integração");
 }
-
-function simularLeituraRFID() {
-
-    const tag =
-        "RFID-" +
-        Math.floor(
-            Math.random() * 10000
-        );
-
-    pecas.push({
-        codigo: tag,
-        descricao: "Peça RFID",
-        quantidade: 1
-    });
-
-    salvarDados();
-    atualizarTabela();
-
-    console.log(
-        "TAG lida:",
-        tag
-    );
-}
-
-/* Simula leitura a cada 30 segundos */
-
-setInterval(
-    simularLeituraRFID,
-    30000
-);

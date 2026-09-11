500838029963f55d96f194f4f68842e4ea80227a
DesenvolvedorDWK <111389400+DesenvolvedorDWK@users.noreply.github.com>
2026-09-11 11:39:15 -0300

    Update api.js

--- api.js
+++ api.js
@@ -9,7 +9,7 @@ async function consultarIA() {
 
         const resposta =
         await fetch(
-            "https://SUA_API_AQUI.com",
+            "https://use.ai/pt?model=gpt-5.6&utm_source=bing&utm_medium=cpc&utm_campaign=AI_BR_PT_Chat_DSK_SEA_LLM_ChatGPT&utm_campaign_id=524286085&utm_adgroup=AI_BR_PT_ChatGPT_Exact&utm_adgroup_id=1326013930979676&utm_term=chat+gpt&utm_match_type=e&utm_content=&utm_content_id=&utm_funnel=&partner=WM&url=https%3A%2F%2Fuse.ai%2Fpt%3Fmodel%3Dgpt-5.6&msclkid=f807bc25a0a4179f4922d681b2cdd160",
             {
                 method: "POST",
 
